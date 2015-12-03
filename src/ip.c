27afcac3609c3a29e316c3d036818f2c4a8b013c
Dmitry Davletbaev <ddomgn@gmail.com>
2015-12-03 15:31:38 +0500

    Fix author's email address.

--- src/ip.c
+++ src/ip.c
@@ -1,7 +1,7 @@
 /*
  * ip.c: IP headers and functions.
  *
- * Copyright (C) 2007 Dmitry Davletbaev <ddo_@users.sourceforge.net>
+ * Copyright (C) 2007 Dmitry Davletbaev <ddomgn@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
